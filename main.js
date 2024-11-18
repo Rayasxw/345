757fd409894508f926abfd011b9342874a2ce287
Rayasxw <maksatbekovarayana@gmail.com>
2024-11-18 14:24:18 +0600

    first-commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+
+  var assessment = Number(prompt("Введите оценку от 1 до 10")).trim();
+  console.log(assessment);
+
+
+  if(assessment >= 5){
+  alert("Спасибо за высокую оценку!")
+  }else if (assessment < 5){
+  alert("Мы работаем над улучшением сервиса!")
+  }else{
+  alert('Введите оценку от 1 до 10')
+  }
+
+    
+  
